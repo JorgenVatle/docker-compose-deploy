4b5a082c1b33ee6e01a5eee5d4b79867d2e76b62
Jørgen Vatle <Jorgen@WcAServices.net>
2020-02-20 23:03:32 +0200

    Add InjectionTargets type.

---
+++ src/Interfaces/Injections.ts
@@ -0,0 +1,5 @@
+/**
+ * Template injection targets.
+ * These are variables that should be injected straight into bash scripts.
+ */
+export type InjectionTargets = 'CONTAINER_VALIDATION_TARGETS' | 'REPO_NAME' | 'COMPOSE_FILE';
