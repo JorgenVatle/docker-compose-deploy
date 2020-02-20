be2c114f83c813d3776045ad699409d4fd4633ca
Jørgen Vatle <Jorgen@WcAServices.net>
2020-02-20 23:03:56 +0200

    Use singular name for InjectionTargets type.

--- src/Interfaces/Injections.ts
+++ src/Interfaces/Injections.ts
@@ -2,4 +2,4 @@
  * Template injection targets.
  * These are variables that should be injected straight into bash scripts.
  */
-export type InjectionTargets = 'CONTAINER_VALIDATION_TARGETS' | 'REPO_NAME' | 'COMPOSE_FILE';
+export type InjectionTarget = 'CONTAINER_VALIDATION_TARGETS' | 'REPO_NAME' | 'COMPOSE_FILE';
