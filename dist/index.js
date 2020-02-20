2beef147b58e2f0e36fc20f850ee7ca42d0efe9e
Jørgen Vatle <Jorgen@WcAServices.net>
2020-02-21 01:35:05 +0200

    compile package

--- dist/index.js
+++ dist/index.js
@@ -11,9 +11,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = __importDefault(require("child_process"));
-const core_1 = __importDefault(require("@actions/core"));
+const Core = __importStar(require("@actions/core"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const util_1 = require("util");
@@ -41,14 +48,14 @@ function injectIntoTemplate(templatePath, injections) {
  * Take a comma separated string and convert it to an array.
  */
 function getInputAsArray(name) {
-    return core_1.default.getInput(name).split(',').map((entry) => entry.trim());
+    return Core.getInput(name).split(',').map((entry) => entry.trim());
 }
 (() => __awaiter(void 0, void 0, void 0, function* () {
     const DeployTargets = getInputAsArray('deploy_targets');
     const ValidateContainers = getInputAsArray('validate_containers');
-    const SshUser = core_1.default.getInput('ssh_user');
-    const ComposeFile = core_1.default.getInput('compose_file');
-    const JsonConfig = core_1.default.getInput('json_config');
+    const SshUser = Core.getInput('ssh_user');
+    const ComposeFile = Core.getInput('compose_file');
+    const JsonConfig = Core.getInput('json_config');
     const RepoName = process.env.GITHUB_REPOSITORY.replace('.*\/', '');
     yield injectIntoTemplate(Templates.GitPostReceive, [
         {
@@ -64,12 +71,12 @@ function getInputAsArray(name) {
             value: RepoName,
         }
     ]);
-    core_1.default.exportVariable('GITHUB_REPONAME', RepoName);
-    core_1.default.exportVariable('JSON_LOCAL_CONFIG', JsonConfig);
-    core_1.default.exportVariable('DEPLOY_TARGETS', DeployTargets.join(', '));
-    core_1.default.exportVariable('DEPLOY_USER', SshUser);
+    Core.exportVariable('GITHUB_REPONAME', RepoName);
+    Core.exportVariable('JSON_LOCAL_CONFIG', JsonConfig);
+    Core.exportVariable('DEPLOY_TARGETS', DeployTargets.join(', '));
+    Core.exportVariable('DEPLOY_USER', SshUser);
     child_process_1.default.execFileSync(path_1.default.join(__dirname, '../scripts/prepare-deploy.sh'));
     child_process_1.default.execFileSync(path_1.default.join(__dirname, '../scripts/deploy.sh'));
 }))().catch((error) => {
-    core_1.default.setFailed(error.message);
+    Core.setFailed(error.message);
 });
