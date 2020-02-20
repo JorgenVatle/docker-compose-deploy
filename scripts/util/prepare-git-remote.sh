10141eeb8547ceb1e40bc68de227961bde49e4d4
Jørgen Vatle <Jorgen@WcAServices.net>
2020-02-20 22:18:29 +0200

    Add git remote setup utility script.

---
+++ scripts/util/prepare-git-remote.sh
@@ -0,0 +1,24 @@
+ssh "root@$1" << "EOF"
+
+  # Exit if remote repository is already set up
+  [ -d "/opt/$GITHUB_REPONAME.git" ] && exit 0
+
+  # Install Git if not available
+  if ! [ -x "$(command -v git)" ]; then
+    echo 'Error: git is not installed.' >&2
+    yum install -y git
+  fi
+
+  # Source code
+  mkdir -p "/opt/live/$GITHUB_REPONAME"
+
+  # Remote Git repository
+  mkdir -p "/opt/$GITHUB_REPONAME.git"
+
+  # Initialize Git repo
+  cd "/opt/$GITHUB_REPONAME.git"
+  git init --bare
+
+  echo "Successfully set up Git on $1"
+
+EOF
