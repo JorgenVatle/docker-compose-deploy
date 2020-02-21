81268da9e7ba548621d9ef1841c5944d43ad4ce7
Jørgen Vatle <Jorgen@WcAServices.net>
2020-02-21 02:43:25 +0200

    compile package

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -26,6 +17,7 @@ const path_1 = __importDefault(require("path"));
 const util_1 = require("util");
 const readFile = util_1.promisify(fs_1.default.readFile);
 const writeFile = util_1.promisify(fs_1.default.writeFile);
+const execFile = util_1.promisify(child_process_1.default.execFile);
 const __workspace = process.env.GITHUB_WORKSPACE;
 /**
  * Template scripts that require insertion of some sort of data to work correctly.
@@ -44,14 +36,12 @@ const ExecOptions = {
 /**
  * Inject a set of variables into the given template.
  */
-function injectIntoTemplate(templatePath, injections) {
-    return __awaiter(this, void 0, void 0, function* () {
-        let template = yield readFile(templatePath, 'utf8');
-        injections.forEach(({ target, value }) => {
-            template = template.replace(`__${target}__`, value);
-        });
-        yield writeFile(templatePath, template);
+async function injectIntoTemplate(templatePath, injections) {
+    let template = await readFile(templatePath, 'utf8');
+    injections.forEach(({ target, value }) => {
+        template = template.replace(`__${target}__`, value);
     });
+    await writeFile(templatePath, template);
 }
 /**
  * Take a comma separated string and convert it to an array.
@@ -59,14 +49,29 @@ function injectIntoTemplate(templatePath, injections) {
 function getInputAsArray(name) {
     return Core.getInput(name).split(',').map((entry) => entry.trim());
 }
-(() => __awaiter(void 0, void 0, void 0, function* () {
+/**
+ * Stream a child_process' output to console.
+ */
+function streamConsoleOutput(outputGroup) {
+    return ({ stdout, stderr }) => {
+        Core.startGroup(outputGroup);
+        if (stderr) {
+            Core.error(stderr);
+        }
+        if (stdout) {
+            Core.info(stdout);
+        }
+        Core.endGroup();
+    };
+}
+(async () => {
     const DeployTargets = getInputAsArray('deploy_targets');
     const ValidateContainers = getInputAsArray('validate_containers');
     const SshUser = Core.getInput('ssh_user');
     const ComposeFile = Core.getInput('compose_file');
     const JsonConfig = Core.getInput('json_config');
     const RepoName = process.env.GITHUB_REPOSITORY.replace(/.*\//, '');
-    yield injectIntoTemplate(Scripts.GitPostReceive, [
+    await injectIntoTemplate(Scripts.GitPostReceive, [
         {
             target: 'CONTAINER_VALIDATION_TARGETS',
             value: `( ${ValidateContainers.join(', ')} )`
@@ -86,8 +91,8 @@ function getInputAsArray(name) {
     Core.exportVariable('DEPLOY_USER', SshUser);
     child_process_1.default.execSync(`chmod +x ${Scripts.PrepareDeploy} ${Scripts.Deploy}`);
     child_process_1.default.execSync(`cp -r ${path_1.default.join(__dirname, '../scripts')} ${path_1.default.join(__workspace, '.docker-compose-deploy_scripts')}`);
-    child_process_1.default.execFileSync(Scripts.PrepareDeploy, ExecOptions);
-    child_process_1.default.execFileSync(Scripts.Deploy, ExecOptions);
-}))().catch((error) => {
+    await execFile(Scripts.PrepareDeploy, ExecOptions).then(streamConsoleOutput('Prepare deploy'));
+    await execFile(Scripts.Deploy, ExecOptions).then(streamConsoleOutput('Deploy to remote'));
+})().catch((error) => {
     Core.setFailed(error.message);
 });
