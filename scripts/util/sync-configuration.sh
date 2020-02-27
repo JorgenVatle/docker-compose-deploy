10a800ab079c7b48ecbe473cc12d4bed19239af0
Jørgen Vatle <Jorgen@WcAServices.net>
2020-02-27 06:48:02 +0200

    Prepare config directory before syncing it in sync-configuration.sh

--- scripts/util/sync-configuration.sh
+++ scripts/util/sync-configuration.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 # Sync current config directory with deploy target.
-rsync -r ../../config/ "$DEPLOY_USER@$1:/opt/live/$GITHUB_REPONAME/config/"
+target="/opt/live/$GITHUB_REPONAME/config/"
+ssh "$DEPLOY_USER@$1" mkdir -p "$target"
+rsync -r ../../config/ "$DEPLOY_USER@$1:$target"
