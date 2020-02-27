c1b4bc69e4da7c20ca0b0f4ec3a1e311ff67d992
Jørgen Vatle <Jorgen@WcAServices.net>
2020-02-27 06:44:12 +0200

    Create Git hooks directory before syncing them in sync-git-hooks script.

--- scripts/util/sync-git-hooks.sh
+++ scripts/util/sync-git-hooks.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+ssh "$DEPLOY_USER@$1" mkdir -p "/opt/$GITHUB_REPONAME.git/hooks/"
 rsync -r ../git-hooks/ "$DEPLOY_USER@$1:/opt/$GITHUB_REPONAME.git/hooks/"
 ssh "$DEPLOY_USER@$1" chmod ug+x /opt/$GITHUB_REPONAME.git/hooks/post-receive
