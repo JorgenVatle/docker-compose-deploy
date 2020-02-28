e39c17c50aded902e5e68c61d9d68c51174054c0
Jørgen Vatle <Jorgen@WcAServices.net>
2020-02-28 03:42:58 +0200

    Set up multi-destination git remote setup.

--- scripts/util/add-deploy-targets.sh
+++ scripts/util/add-deploy-targets.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-git remote add live "ssh://$DEPLOY_USER@$1/opt/$GITHUB_REPONAME.git" & exit 0
+
+# Target deploy remote
+remote=live
+target="ssh://$DEPLOY_USER@$1/opt/$GITHUB_REPONAME.git"
+
+if git config "remote.$remote.url" > /dev/null; then
+    # Remote exists, add additional remote
+    git remote set-url --add --push "$remote" "$target"
+    echo "Added $1 to $remote remote."
+else
+    # No remote exists, create one
+    git remote add "$remote" "$target"
+    git remote set-url --add --push "$remote" "$target"
+    echo "Created $remote remote for $1."
+fi
